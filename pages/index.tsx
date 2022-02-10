565cfdd345d0e8df6e6c3cb5ba66bf2f76f1b3a9
Dmitry Bludchy <dmitrybludchy@gmail.com>
2022-02-10 19:56:40 +0300

    add component button, svg-config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
-import { Htag } from '../components';
+import { Htag, Button } from '../components';
 
 const Home: NextPage = () => {
   return (
     <>
       <Htag tag='h1'>Text</Htag>
+      <Button appearance='primary' arrow='right'>Кнопка</Button>
+      <Button appearance='ghost' arrow='right'>Кнопка</Button>
     </>
   );
 };
