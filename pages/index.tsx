cb5e2cef072094ef822b68b96328fe77a6c563de
Dmitry Bludchy <dmitrybludchy@gmail.com>
2022-02-10 21:18:33 +0300

    add component tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { Htag, Button, P } from '../components';
+import { Htag, Button, P, Tag } from '../components';
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +10,10 @@ const Home: NextPage = () => {
       <P size='l'>Большой</P>
       <P>Средний</P>
       <P size='s'>Маленький</P>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag size='s' color='primary'>Primary</Tag>
     </>
   );
 };
