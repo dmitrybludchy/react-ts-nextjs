5f20cef6141f335924ff63543569ece2803c9d0c
Dmitry Bludchy <dmitrybludchy@gmail.com>
2022-02-12 16:21:52 +0300

    footer style

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -4,8 +4,8 @@ import cn from 'classnames';
 
 export const Header = ({ ...props }: HeaderProps): JSX.Element => {
     return (
-        <div {...props}>
+        <header {...props}>
             Header
-        </div>
+        </header>
     );
 };
